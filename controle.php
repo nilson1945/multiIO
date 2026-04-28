fd71f59ee8ba15e1c2afeb6ca62182da99284ae9
Nilson Souza <pupet1943@gmail.com>
2026-04-28 07:47:56 -0300

    Fix URL path in controle.php

--- controle.php
+++ controle.php
@@ -6,7 +6,7 @@ $address = isset($_GET['address']) ? (int)$_GET['address'] : 0;
 $state   = isset($_GET['state']) ? (int)$_GET['state'] : 0;
 $time_1  = isset($_GET['time_1']) ? (int)$_GET['time_1'] : 0;
 
-$url = "http://localhost/MULTIIO/fake_mio.php?address={$address}&state={$state}";
+$url = "http://localhost/fake_mio.php?address={$address}&state={$state}";
 
 if($time_1 > 0){
     $url .= "&time_1={$time_1}";
